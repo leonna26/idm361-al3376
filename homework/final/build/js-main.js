ba6bd02e24aae12c0877e10f68f137a8012fa6e0
Anna Leong <al3376@drexel.edu>
2020-12-10 20:21:36 -0500

    Updated

--- homework/final/build/js-main.js
+++ homework/final/build/js-main.js
@@ -316,7 +316,26 @@ function render() {
 
     lifePercentage.innerHTML = '' + getLifePercentage().toFixed(0) + '%';
     //  */ // lifeProgressBar.value = Math.floor(getLifePercentage());
+    yearPercentageLandscape.innerHTML = '' + getYearPercentageLandscape().toFixed(0) + '%';
+    var lifePercentageNum = getLifePercentage().toFixed(0);
+    console.log("life percentage is equal to " + lifePercentageNum);
+    var percentageRemainNum = (100 - lifePercentageNum);
+    console.log("percentage remaining is equal to " + percentageRemainNum);
+    if (lifePercentageNum > 0 && percentageRemainNum > 0) {
+        buildChart(lifePercentageNum, percentageRemainNum);
+    }
+
+    //  */ // progressBar.value = Math.floor(getYearPercentage());
+
+    dayPercentageLandscape.innerHTML = '' + getDayPercentageLandscape().toFixed(0) + '%';
+    // dayProgressBar.value = Math.floor(getDayPercentage());
+    monthPercentageLandscape.innerHTML = '' + getMonthPercentageLandscape().toFixed(0) + '%';
+
+    lifePercentageLandscape.innerHTML = '' + getLifePercentageLandscape().toFixed(0) + '%';
+    //  */ // lifeProgressBar.value = Math.floor(getLifePercentage());
 }
+
+
 
 
 var yearPercentageLandscape = document.getElementById('year-percentage-landscape');
@@ -383,21 +402,17 @@ function getLifePercentageLandscape() {
     return (secondsInYourLife / secondsInAverageLife) * 100;
 }
 
-
+/* 
 function render() {
     yearPercentageLandscape.innerHTML = '' + getYearPercentageLandscape().toFixed(0) + '%';
     //  */ // progressBar.value = Math.floor(getYearPercentage());
 
-    dayPercentageLandscape.innerHTML = '' + getDayPercentageLandscape().toFixed(0) + '%';
-    // dayProgressBar.value = Math.floor(getDayPercentage());
-    monthPercentageLandscape.innerHTML = '' + getMonthPercentageLandscape().toFixed(0) + '%';
+dayPercentageLandscape.innerHTML = '' + getDayPercentageLandscape().toFixed(0) + '%';
+// dayProgressBar.value = Math.floor(getDayPercentage());
+monthPercentageLandscape.innerHTML = '' + getMonthPercentageLandscape().toFixed(0) + '%';
 
-    lifePercentageLandscape.innerHTML = '' + getLifePercentageLandscape().toFixed(0) + '%';
-    //  */ // lifeProgressBar.value = Math.floor(getLifePercentage());
-}
-
-
-
+lifePercentageLandscape.innerHTML = '' + getLifePercentageLandscape().toFixed(0) + '%';
+//  */ // lifeProgressBar.value = Math.floor(getLifePercentage());
 
 
 function getDaysInMonth(month, year) {
@@ -411,7 +426,56 @@ function getDaysInMonth(month, year) {
 
 
 function start() {
-    setInterval(function() { render(); }, 1000);
+    setInterval(function() { render(); }, );
 }
 
 start();
+
+// buildChart(40, 60);
+
+function buildChart(userSlice, leftoverSlice) {
+    // set the dimensions and margins of the graph
+    var width = 320
+    height = 320
+    margin = 40
+
+    // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+    var radius = Math.min(width, height) / 2 - margin
+
+    // append the svg object to the div called 'my_dataviz'
+    var svg = d3.select("#my_dataviz")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    // Create dummy data
+    var data = { a: userSlice, b: leftoverSlice }
+
+    // set the color scale
+    var color = d3.scaleOrdinal()
+        .domain(data)
+        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"])
+
+    // Compute the position of each group on the pie:
+    var pie = d3.pie()
+        .value(function(d) { return d.value; })
+    var data_ready = pie(d3.entries(data))
+
+    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+    svg
+        .selectAll('whatever')
+        .data(data_ready)
+        .enter()
+        .append('path')
+        .attr('d', d3.arc()
+            .innerRadius(0)
+            .outerRadius(radius)
+        )
+        .attr('fill', function(d) { return (color(d.data.key)) })
+        .attr("stroke", "black")
+        .style("stroke-width", "2px")
+        .style("opacity", 0.7)
+
+}
